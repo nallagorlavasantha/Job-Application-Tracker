492748fee2bef9ca5366ec7c13e8593a71fff00f
vasuglory <nallagorlavasantha@gmail.com>
2026-01-17 23:58:34 +0530

    design and code for home page is complete

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const port = 2000;
+const app = express();
+app.use(express.static('public'));
+
+app.get("/",(_req,res)=>{
+  res.render("home.ejs");
+})
+app.listen(port,()=>{
+  console.log(`server started on port, ${port}`);
+});
