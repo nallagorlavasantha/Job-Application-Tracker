bf0004bad5154dc6580154d52047f58a16626363
vasuglory <nallagorlavasantha@gmail.com>
2026-01-24 22:34:06 +0530

    feat: Add responsive form - Added styled inputs and labels - Improved mobiled and desktop responsiveness

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/",(_req,res)=>{
+  res.redirect("/JobTracker");
+});
+
+
+app.get("/JobTracker",(_req,res)=>{
   res.render("home.ejs");
-})
+});
 app.listen(port,()=>{
   console.log(`server started on port, ${port}`);
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,18 @@
+const addAJobBtn = document.getElementById("add-job-btn");
+const formContainer = document.querySelector(".form-container");
+const overlay = document.querySelector(".overlay");
+addAJobBtn.addEventListener("click",()=>{
+    console.log("form activated");
+    // document.querySelector("body").style.opacity = 0.5;
+    
+    const overlay = document.querySelector(".overlay");
+    overlay.classList.add("overlay-active");
+    formContainer.classList.add("form-container-display");
+})
+
+//candel add a jon form
+const cancelForm = document.getElementById("cancel-btn");
+cancelForm.addEventListener("click",()=>{
+    formContainer.classList.remove("form-container-display");
+    overlay.classList.remove("overlay-active");
+})
